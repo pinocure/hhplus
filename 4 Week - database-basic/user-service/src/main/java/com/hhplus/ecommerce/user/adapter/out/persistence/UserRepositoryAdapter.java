0e3170aa1cf2b648275f865e64c7dc07066c0156
pinocure <xodiq@naver.com>
2025-07-30 23:40:30 +0900

    feat: user-service JPA & 통합테스트

--- 4 Week - database-basic/user-service/src/main/java/com/hhplus/ecommerce/user/adapter/out/persistence/UserRepositoryAdapter.java
+++ 4 Week - database-basic/user-service/src/main/java/com/hhplus/ecommerce/user/adapter/out/persistence/UserRepositoryAdapter.java
@@ -2,6 +2,7 @@ package com.hhplus.ecommerce.user.adapter.out.persistence;
 
 import com.hhplus.ecommerce.user.application.port.out.UserRepository;
 import com.hhplus.ecommerce.user.domain.User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
@@ -10,28 +11,26 @@ import java.util.Optional;
 
 /**
  * 역할: user 리포지토리 어댑터 클래스 (Outbound Adapter)
- * 책임: UserRepository를 구현하며, JPA 등 실제 DB와 연결하여 데이터 영속화
+ * 책임: UserRepository를 구현하며, JPA 등 실제 DB와 연결하여 데이터 영속화 및 도메인-JPA 엔티티 간 변환
 */
 
 @Repository
+@RequiredArgsConstructor
 public class UserRepositoryAdapter implements UserRepository {
 
-    private final Map<Long, User> store = new HashMap<>();
-
-    public UserRepositoryAdapter() {
-        store.put(1L, new User(1L));
-    }
-
+    private  final UserJpaRepository userJpaRepository;
 
     @Override
     public Optional<User> findById(Long userId) {
-        return Optional.ofNullable(store.get(userId));
+        return userJpaRepository.findById(userId)
+                .map(UserJpaEntity::toDomain);
     }
 
     @Override
     public User save(User user) {
-        store.put(user.getId(), user);
-        return user;
+        UserJpaEntity entity = UserJpaEntity.from(user);
+        UserJpaEntity saved = userJpaRepository.save(entity);
+        return saved.toDomain();
     }
 
 }

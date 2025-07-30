1821a4496cd92501c0c48ba26db79d9fc40d449c
pinocure <xodiq@naver.com>
2025-07-31 00:11:34 +0900

    feat: balance-service JPA & 통합테스트

---
+++ 4 Week - database-basic/balance-service/src/main/java/com/hhplus/ecommerce/balance/adapter/out/persistence/BalanceJpaRepository.java
@@ -0,0 +1,14 @@
+package com.hhplus.ecommerce.balance.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * 역할: Balance JPA Repository 인터페이스
+ * 책임: Spring Data JPA를 통해 잔액 데이터의 실제 DB 접근을 담당
+ */
+
+public interface BalanceJpaRepository extends JpaRepository<BalanceJpaEntity, Long> {
+
+
+
+}

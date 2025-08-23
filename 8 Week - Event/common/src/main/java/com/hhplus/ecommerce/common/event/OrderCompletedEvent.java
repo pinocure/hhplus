e1a7c9e3dc7f4427e43f73e7b23a984209498e35
pinocure <xodiq@naver.com>
2025-08-23 22:04:59 +0900

    feat: Add event class

---
+++ 8 Week - Event/common/src/main/java/com/hhplus/ecommerce/common/event/OrderCompletedEvent.java
@@ -0,0 +1,48 @@
+package com.hhplus.ecommerce.common.event;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class OrderCompletedEvent {
+
+    private final Long orderId;
+    private final Long userId;
+    private final BigDecimal totalPrice;
+    private final LocalDateTime orderedAt;
+
+
+    public OrderCompletedEvent(Long orderId, Long userId, BigDecimal totalPrice, LocalDateTime orderedAt) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.totalPrice = totalPrice;
+        this.orderedAt = orderedAt;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public LocalDateTime getOrderedAt() {
+        return orderedAt;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
